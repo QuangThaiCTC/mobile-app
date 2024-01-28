bbf580c6a77c6fb8877166fe686c1b6317f8e5cb
QuangThaiCTC <quangthaictc@gmail.com>
2024-01-28 09:29:15 +0700

    Thiet ke giao dien

---
+++ app/src/main/java/com/school/mobileappcuthair/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.school.mobileappcuthair;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
